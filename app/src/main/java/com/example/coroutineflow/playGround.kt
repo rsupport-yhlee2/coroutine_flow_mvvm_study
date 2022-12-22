96406834cf766b7e1d9bdda0e973520070a3a4c3
yhlee2 <yhlee2@rsupport.com>
2022-12-22 12:56:55 +0900

    playground mvvm loading logic by coroutine 구현

--- app/src/main/java/com/example/coroutineflow/playGround.kt
+++ app/src/main/java/com/example/coroutineflow/playGround.kt
@@ -3,58 +3,74 @@ package com.example.coroutineflow
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import java.util.Random
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.system.measureTimeMillis
 
-val stateFlow = MutableStateFlow("")
+//in viewmodel
+
 //ui
 //먼저 완료된 network 리퀘스트부터 ui에 뿌려준다.
 fun main() {
     runBlocking {
-        onViewModel()
-        //state -> userComplete, profileComplete, friendComplete, chatComplete
-        stateFlow.collect{ job ->
+        var progress = 0
+        onViewModel().map {
+            "${it} Request Complete!"
+        }.collect { job ->
             println(job)
+            progress += 25
+            println("$progress %")
         }
     }
 }
 
-fun onViewModel() = runBlocking {
-    val time = measureTimeMillis {
-        val userJob = async {
-            userRequest()
+//viewModel
+fun onViewModel() = channelFlow {
+    runBlocking {
+        val totalTime = measureTimeMillis {
+            val networkRequest = launch {
+                launch { send(userRequest()) }
+                launch { send(profileRequest()) }
+                launch { send(friendsRequest()) }
+                launch { send(chatRequest()) }
+            }
+            //먼저 완료된 job 부터 출력 가능하게
+            networkRequest.join()
         }
-        val profileJob = async {
-            profileRequest()
-        }
-        val friendsJob = async {
-            friendsRequest()
-        }
-        val chatJob = async {
-            chatRequest()
-        }
-        //먼저 완료된 job 부터 출력 가능하게
-
+        println("Loading Complete!! start from $totalTime ms...")
     }
-    println(time)
-}
+}.flowOn(Dispatchers.IO)
 
+//repository
 suspend fun userRequest(): String {
-    delay(500)
+    val time = measureTimeMillis {
+        delay((1..10000).random().toLong())
+    }
+    println("userRequest: $time ms...")
     return "userinfo"
 }
 
 suspend fun profileRequest(): String {
-    delay(1000)
+    val time = measureTimeMillis {
+        delay((1..10000).random().toLong())
+    }
+    println("profileRequest: $time ms...")
     return "profileImage"
 }
 
 suspend fun friendsRequest(): String {
-    delay(1500)
+    val time = measureTimeMillis {
+        delay((1..10000).random().toLong())
+    }
+    println("friendsRequest: $time ms...")
     return "friends"
 }
 
 suspend fun chatRequest(): String {
-    delay(1800)
+    val time = measureTimeMillis {
+        delay((1..10000).random().toLong())
+    }
+    println("chatRequest: $time ms...")
     return "chatInfo"
 }
 
